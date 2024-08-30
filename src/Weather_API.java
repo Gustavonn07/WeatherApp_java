1fc5f0bc9486d65b85a6dfda9a32f0d6c5964a1f
Gustavonn07 <gustavonepnog@gmail.com>
2024-08-30 16:57:24 -0300

    add: adicionando methods API

--- src/Weather_API.java
+++ src/Weather_API.java
@@ -6,12 +6,69 @@ import java.io.IOException;
 import java.net.HttpRetryException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Weather_API {
 
     public static JSONObject getWeatherData(String locationName) {
         JSONArray locationData = getLocationData(locationName);
+
+        JSONObject location = (JSONObject) locationData.get(0);
+        double latitude = (double) location.get("Latitude");
+        double longitude = (double) location.get("Longitude");
+
+        final String URL_STRING = "https://api.open-meteo.com/v1/forecast?" +
+                "latitude=" + latitude + "&longitude=" + longitude +
+                "&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&timezone=America%2FLos_Angeles";
+
+        try {
+            HttpURLConnection conn = fetchApiResponse(URL_STRING);
+
+            if(conn.getResponseCode() != 200) {
+                System.out.println("Error: could not connect to API");
+                return null;
+            }
+
+            StringBuilder resultJSON = new StringBuilder();
+            Scanner scanner = new Scanner(conn.getInputStream());
+            while(scanner.hasNext()) resultJSON.append(scanner.nextLine());
+
+            scanner.close();
+            conn.disconnect();
+
+            JSONParser parser = new JSONParser();
+            JSONObject resultJsonObject = (JSONObject) parser.parse(String.valueOf(resultJSON));
+            JSONObject hourly = (JSONObject) resultJsonObject.get("hourly");
+
+            JSONArray time = (JSONArray) hourly.get("time");
+            int index = findIndexOfCurrentTime(time);
+
+            JSONArray temperatureData = (JSONArray) hourly.get("temperature_2m");
+            double temperature = (double) temperatureData.get(index);
+
+            JSONArray weathercode = (JSONArray) hourly.get("weathercode");
+            String weatherCondition = convertWeatherCode((long) weathercode.get(index));
+
+            JSONArray relativeHumidity = (JSONArray) hourly.get("relativehumidity_2m");
+            long humidity = (long) relativeHumidity.get(index);
+
+            JSONArray windspeedData = (JSONArray) hourly.get("windspeed_10m");
+            double windspeed = (double) windspeedData.get(index);
+
+            JSONObject weatherData = new JSONObject();
+            weatherData.put("temperature", temperature);
+            weatherData.put("weather_condition", weatherCondition);
+            weatherData.put("humidity", humidity);
+            weatherData.put("windspeed", windspeed);
+
+            return weatherData;
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
 
         return null;
     }
@@ -64,5 +121,40 @@ public class Weather_API {
         }
 
         return null;
+    }
+
+    private static int findIndexOfCurrentTime(JSONArray timeList) {
+        String currentTime = getCurrentTime();
+
+        for(int i = 0; i < timeList.size(); i++) {
+            String time = (String) timeList.get(i);
+            if(time.equalsIgnoreCase(currentTime)) return i;
+        }
+
+        return 0;
+    }
+
+    private static String getCurrentTime() {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        //AAAA-DD-MMTHH:MM
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH':00'");
+        String formattedDateTime = currentDateTime.format(formatter);
+        return formattedDateTime;
+    }
+
+    private static String convertWeatherCode(long weathercode) {
+        String weatherCondition = "";
+
+        if(weathercode == 0L)weatherCondition = "Clear";
+
+        if(weathercode > 0L && weathercode <= 3L) weatherCondition = "Cloudy";
+
+        if((weathercode >= 51L && weathercode <= 67L)
+                || (weathercode >= 80L && weathercode <= 99L)) weatherCondition = "Rain";
+
+        if(weathercode >= 71L && weathercode <= 77L) weatherCondition = "Snow";
+
+        return weatherCondition;
     }
 }
